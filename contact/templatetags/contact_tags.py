e64171da664a9f148273b81841104f5dbb2c9d4c
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2021-05-17 11:00:33 +0100

    cleanup

--- contact/templatetags/contact_tags.py
+++ contact/templatetags/contact_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+from ..models import ContactInfo
 from ..forms import ContactForm
 
 register = template.Library()
@@ -8,3 +9,9 @@ register = template.Library()
 def get_contact_form():
     contact_form = ContactForm()
     return contact_form
+
+@register.simple_tag
+def get_contact_info():
+    contact = ContactInfo.objects.first() or ""
+
+    return contact
